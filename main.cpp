cb574d214279784fb80704bb9960432456683da4
Aaron & Kenneth <166048423+AaronAndKenneth@users.noreply.github.com>
2024-04-30 16:17:19 +0000

    completed display sales

--- main.cpp
+++ main.cpp
@@ -1,21 +1,25 @@
-//input: data from calculate sales
-//process:
-//output: display to user gross sales for day, week, month
 #include <iostream>
+#include "Vehicle.h"
+#include "Customer.h"
 using namespace std;
+
+//Input: Vehicle and custmer data read from file. User input to navigate menus to search. MD
+//Process: Files are read and vectors of vehicles and customers are made. The user navitages menus to search for vehicles and customers based on specific data values. Sales data can also be searched. MD
+//Output: The program outputs the result of the user's search. MD
+void DisplaySales(int salesDay, int salesWeek, int salesMonth);
+
+int main() {
+    int salesDay, salesWeek, salesMonth = 0;
+    DisplaySales(salesDay, salesWeek, salesMonth);
+
+
+    return 1;
+  
+}//end main
 
 void DisplaySales(int salesDay, int salesWeek, int salesMonth) {
     cout << "Sales today: $" << salesDay << endl;
     cout << "Sales this week: $" << salesWeek << endl;
     cout << "Sales this month: $" << salesMonth << endl;
 
-}
-
-int main() {
-    int salesDay, salesWeek, salesMonth = 0;
-    // Does calculateSales update above vars?
-    DisplaySales(salesDay, salesWeek, salesMonth);
-
-    return 1;
-
-}
+} 
