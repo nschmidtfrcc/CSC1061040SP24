0af92b0f4e85fcff17830750609392fb8312c149
Jack <sparkdragon586@gmail.com>
2024-04-30 18:41:53 -0600

    reorganized main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-
+#include "Vehicle.h"
+#include "Customer.h"
 using namespace std;
 
 int displayMenu();
 
-#include "Vehicle.h"
-#include "Customer.h"
-using namespace std;
 
 //Input: Vehicle and custmer data read from file. User input to navigate menus to search. MD
 //Process: Files are read and vectors of vehicles and customers are made. The user navitages menus to search for vehicles and customers based on specific data values. Sales data can also be searched. MD
