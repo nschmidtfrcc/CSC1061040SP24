d4b08d0e8270bfb30f547440db158892d8e18b24
OscarNConnor <166048428+OscarNConnor@users.noreply.github.com>
2024-04-30 18:47:01 -0600

    Oscar and Connor (#8)
    * First Day
    
    * please
    
    * reorganized main
    
    ---------
    
    Co-authored-by: OscarNConnor <ccollier8@student.cccs.edu>
    Co-authored-by: sparkdragon586 <42307022+sparkdragon586@users.noreply.github.com>
    Co-authored-by: Jack <sparkdragon586@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include "Customer.h"
 using namespace std;
 
+int displayMenu();
+
+
 //Input: Vehicle and custmer data read from file. User input to navigate menus to search. MD
 //Process: Files are read and vectors of vehicles and customers are made. The user navitages menus to search for vehicles and customers based on specific data values. Sales data can also be searched. MD
 //Output: The program outputs the result of the user's search. MD
@@ -12,3 +15,32 @@ int main() {
 
   
 }//end main
+
+
+int displayMenu() {
+
+//I: User inputs number corresponding to the 3 different choices; searchInventory, displaySales, or customerList
+//P: Validate input
+//O: Output user choice
+
+int userChoice = 0;
+   
+   cout << "Welcome to the Front Range Community Car Lot Database!" << endl; 
+   cout << "| Would you like to search:                          |" << endl;
+   cout << "| 1. Inventory                                       |" << endl;
+   cout << "| 2. Sales Data                                      |" << endl;
+   cout << "| 3. List of Customers                               |" << endl << endl;
+   cout << "Enter Choice: ";
+   
+   cin >> userChoice;
+    
+   if (userChoice < 1 || userChoice  > 3) {
+      cout << endl << "Please input a valid number" << endl;
+   }else{
+      return userChoice;
+   }//end if-else
+   
+   return 0;
+   
+}//end displayMenu
+
