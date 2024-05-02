30aa864d1fc52610d6956731597fb35306248a89
Aaron & Kenneth <166048423+AaronAndKenneth@users.noreply.github.com>
2024-05-01 19:16:15 -0600

    Created display sales (#9)
    * created our main
    
    * completed display sales
    
    ---------
    
    Co-authored-by: Jack <sparkdragon586@gmail.com>

--- main.cpp
+++ main.cpp
@@ -6,16 +6,25 @@ using namespace std;
 int displayMenu();
 
 
+void DisplaySales(int salesDay, int salesWeek, int salesMonth);
+
+
 //Input: Vehicle and custmer data read from file. User input to navigate menus to search. MD
 //Process: Files are read and vectors of vehicles and customers are made. The user navitages menus to search for vehicles and customers based on specific data values. Sales data can also be searched. MD
 //Output: The program outputs the result of the user's search. MD
 int main() {
-
-
+    int salesDay, salesWeek, salesMonth = 0;
+    DisplaySales(salesDay, salesWeek, salesMonth);
 
   
 }//end main
 
+void DisplaySales(int salesDay, int salesWeek, int salesMonth) {
+    cout << "Sales today: $" << salesDay << endl;
+    cout << "Sales this week: $" << salesWeek << endl;
+    cout << "Sales this month: $" << salesMonth << endl;
+
+} 
 
 int displayMenu() {
 
